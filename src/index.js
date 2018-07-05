fbd935af776234b3a01e836bbfa405aebc99eafb
luis murillo <lumx2@hotmail.com>
2018-07-04 21:16:40 -0500

    Implement Realtime with supscriptions: - config subscriptions adding websocketlink apollolink - config the instance of websocketlink and the apollolink - adding subscription function newLinks - calling the function newlinks in componentdidmount - adding subscription function newLinks and calling it in the componentDidMount

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { ApolloLink, split } from 'apollo-client-preset'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 //1
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { BrowserRouter } from 'react-router-dom';
-import { ApolloLink } from 'apollo-link';
 import { AUTH_TOKEN } from './constants';
 // 2
 const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
@@ -25,14 +27,33 @@ const middlewareAuthLink = new ApolloLink((operation, forward) => {
   })
   return forward(operation)
 })
+
 const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink)
 
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    },
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLinkWithAuthToken,
+)
 // 3
 const client = new ApolloClient({
-    link: httpLinkWithAuthToken,
-    cache: new InMemoryCache()
-  })
-  
+  link,
+  cache: new InMemoryCache(),
+})
+
   // 4
   ReactDOM.render(
     <BrowserRouter>
