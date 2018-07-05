fa0ca096a31d4a45e462b6a241862859ec221c29
luis murillo <lumx2@hotmail.com>
2018-07-04 22:41:07 -0500

    add pagination to LinkList

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,8 +11,10 @@ class Header extends Component {
         <div className="flex flex-fixed black">
           <div className="fw7 mr1">Hacker News</div>
           <Link to="/" className="ml1 no-underline black">
-            all
+            news
           </Link>
+          <div className="ml1">|</div>
+          <Link to='/top' className="ml1 no-underline black"></Link>
           <div className="ml1">|</div>
           <Link to="/search"  className="ml1 no-underline black">
             search
@@ -21,7 +23,7 @@ class Header extends Component {
             <div className="flex">
               <div className="ml1">|</div>
               <Link to="/create" className="ml1 no-underline black">
-                new
+                create
               </Link>  
             </div>
           )} 
